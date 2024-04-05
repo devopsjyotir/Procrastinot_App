8cf4900799fdcdaf86792ac1a4ad41733643ac81
devopsjyotir <jyotirjy@gmail.com>
2024-04-05 07:26:19 -0400

    corrected unresolved placeholder for the alignment parameter in the frame modifier

--- Procrastinot_App/+View.swift
+++ Procrastinot_App/+View.swift
@@ -10,6 +10,6 @@ import SwiftUI
 extension View {
     @ViewBuilder
     func hSpacing(_ alignment: Alignment) -> some View{
-        self.frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: <#T##Alignment#>)
+        self.frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
     }
 }
