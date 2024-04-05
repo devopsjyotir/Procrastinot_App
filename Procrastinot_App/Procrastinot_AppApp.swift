b9be5e3ddfc89afd2dc346fa421f9a799b67af9c
devopsjyotir <jyotirjy@gmail.com>
2024-04-05 06:12:59 -0400

    Initial Commit

---
+++ Procrastinot_App/Procrastinot_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Procrastinot_AppApp.swift
+//  Procrastinot_App
+//
+//  Created by Jyotir McEwan-Anamoah on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Procrastinot_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Procrastinot_App/Task.swift
@@ -0,0 +1,18 @@
+//
+//  Task.swift
+//  Procrastinot_App
+//
+//  Created by Jyotir McEwan-Anamoah on 4/5/24.
+//
+
+import SwiftUI
+
+struct Task: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Task()
+}
