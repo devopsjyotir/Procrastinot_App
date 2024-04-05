4da0024b8a6855c8e68e9fa5bfcdbdf45799a8ff
devopsjyotir <jyotirjy@gmail.com>
2024-04-05 07:19:16 -0400

    formatted Date objects into strings with custom formats.

--- Procrastinot_App/DateExtensions.swift
+++ Procrastinot_App/DateExtensions.swift
@@ -5,4 +5,13 @@
 //  Created by Jyotir McEwan-Anamoah on 4/5/24.
 //
 
+import SwiftUI
 
+extension Date{
+    func format(_ format:String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        
+        return formatter.string(from: self)
+    }
+}
