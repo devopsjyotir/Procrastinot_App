c5f9c961aa818ae76f821cb9fa8b469c1178aaf9
devopsjyotir <jyotirjy@gmail.com>
2024-04-05 06:49:04 -0400

    Add Task model and sample data initialization

--- Procrastinot_App/Task.swift
+++ Procrastinot_App/Task.swift
@@ -7,12 +7,27 @@
 
 import SwiftUI
 
-struct Task: View {
-    var body: some View {
-        Text("Hello, W!")
-    }
+struct Task: Identifiable {
+    var id: UUID = .init()
+    var title: String
+    var caption: String
+    var date: Date = .init()
+    var isCompleted = false
+    var tint: Color
 }
 
-#Preview {
-    Task()
+// Sample Task
+var sampleTask: [Task] = [
+    .init(title: "Wake Up", caption: "wohoo up ", date: Date.now, tint: .yellow),
+    .init(title: "Gym", caption: "Get that work in", date: Date.now, tint: .gray),
+    .init(title: "Ui Design", caption: "Procrastinot App", date: Date.now, tint: .green),
+    .init(title: "Delegate", caption: "Procrastinot App", date: Date.now, tint: .purple),
+]
+
+//Date Extension
+extension Date {
+    static func updateHour(_ value: Int) -> Date {
+        let calendar = Calendar.current
+        return calendar.date(byAdding: .hour, value: value, to: .init()) ?? .init()
+    }
 }
